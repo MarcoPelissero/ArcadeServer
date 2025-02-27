d675b847c0c02448abb9cbca3eeb9a8afb445890
Alessandra <alexandra.zaccaria46@gmail.com>
2025-02-27 14:15:25 +0100

    data service

--- arcadeServer/src/main/java/com/example/arcadeServer/Service/DataService.java
+++ arcadeServer/src/main/java/com/example/arcadeServer/Service/DataService.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.arcadeServer.exception.ResourceNotFoundException;
 import com.example.arcadeServer.model.DataEntity;
 import com.example.arcadeServer.repository.DataRepository;
 
@@ -17,22 +18,26 @@ import com.example.arcadeServer.repository.DataRepository;
 public class DataService {
 
 
-    @Autowired
-    private DataRepository requestRepository;
+	 @Autowired
+	    private DataRepository dataRepository;
 
-    public List<DataEntity> getAllRequests() {
-        return requestRepository.findAll();
-    }
+	    public DataEntity saveRequest(DataEntity dataEntity) {
+	        return dataRepository.save(dataEntity);
+	    }
 
-    public List<DataEntity> getAcceptedRequests() {
-        return requestRepository.findAll().stream()
-                .filter(request -> "accepted".equalsIgnoreCase(request.getStatus()))
-                .collect(Collectors.toList());
-    }
+	    public DataEntity getRequestById(Long id) {
+	        return dataRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Richiesta non trovata"));
+	    }
 
-    public List<DataEntity> getPendingRequests() {
-        return requestRepository.findAll().stream()
-                .filter(request -> "pending".equalsIgnoreCase(request.getStatus()))
-                .collect(Collectors.toList());
-    }
-}
+	    public List<DataEntity> getAllRequests() {
+	        return dataRepository.findAll();
+	    }
+
+	    public List<DataEntity> getAcceptedRequests() {
+	        return dataRepository.findAllById("ACCEPTED");
+	    }
+
+	    public List<DataEntity> getPendingRequests() {
+	        return dataRepository.findAllById("PENDING");
+	    }
+	}
--- arcadeServer/src/main/java/com/example/arcadeServer/repository/DataRepository.java
+++ arcadeServer/src/main/java/com/example/arcadeServer/repository/DataRepository.java
@@ -1,5 +1,7 @@
 package com.example.arcadeServer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.example.arcadeServer.model.DataEntity;
 
 @Repository
 public interface DataRepository extends JpaRepository<DataEntity, Long> {
+    List<DataEntity> findByStatus(String status);
+
+	List<DataEntity> findAllById(String string);
 }
